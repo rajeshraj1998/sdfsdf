e5e2c7498a1480d26c0dbffbaf527366a3c5f575
rajesh <rajeshraj@outlook.com>
2021-07-09 19:59:58 +0530

    namechange

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
 function App() {
     return ( < div >
-        <h2 > Let 's get started!
-            fdgfdgdfg
-            dfg
-            dfg
-            dfgdfg
-            dfg
-            <dfn>gdfg
-                dfg
-                <dfn>gdfggd
-                    fg
-                    <dfn><dfn>gdfgfgd
-                        fg</dfn></dfn></dfn></dfn></h2>{" "}  
-        </div >
+        <h3>dsfdsfgdsfsf</h3>
+
+        </div>
     );
 }
 
